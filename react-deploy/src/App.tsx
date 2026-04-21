68bf21a33707d607543b1fa6b77434898c31eadd
nuwan <nuwansiri.dev@gmail.com>
2026-04-21 20:15:17 +0530

    testing1

--- react-deploy/src/App.tsx
+++ react-deploy/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 const App = () => {
   return (
     <div>
-      <h1>Hello React + AWS to automation</h1>
+      <h1>Hello React + AWS to automation least</h1>
     </div>
   )
 }
